1556c7a1197ce51fffcd8cde86941816ee8f7bd4
L.Boyer <boyerlucas29@gmail.com>
2025-11-17 18:10:49 +0400

    Tween + radar + obstacle

--- Assets/Scripts/radar.cs
+++ Assets/Scripts/radar.cs
@@ -1,7 +1,30 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class Radar : MonoBehaviour
 {
+    public float radiusMin = 0;
+    public float radiusMax = 5;
+    public float scanDurration = 1;
+    bool inRadar = false;
+
+    void Start()
+    {
+        transform.localScale = Vector3.one * radiusMin;
+    }
+    public void UseRadar()
+    {
+        if (inRadar) return;
+        inRadar = true;
+        transform.localScale = Vector3.one * radiusMin;
+        transform.DOScale(radiusMax,scanDurration).OnComplete(EndRadar);
+    }
+
+    private void EndRadar()
+    {
+        inRadar = false;
+        transform.localScale = Vector3.one * radiusMin;
+    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("ObjectBorder"))
