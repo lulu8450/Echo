eb9824689d01aba4a62b09c2a3bce7db4b72ca53
IluminaWolf <injiraky772@gmail.com>
2025-11-18 10:27:07 +0400

    new

--- Assets/ScriptMathieu/MenuController.cs
+++ Assets/ScriptMathieu/MenuController.cs
@@ -1,19 +1,41 @@
 using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MenuController : MonoBehaviour
 {
+
+    public GameObject settingPanel;
+
     public void OnStartClick()
     {
-        SceneManager.LoadScene("SimpleScene");
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OnSettingClick()
+    {
+        settingPanel.SetActive(true);
+    }
+
+    public void OnCloseSettingClick()
+    {
+               settingPanel.SetActive(false);
+    }
+
+    public void OnLinkClick()
+    {
+        Application.OpenURL("https://www.avh.asso.fr/nos-solutions/accueillir-informer-conseiller/sante-des-yeux/cest-quoi-etre-malvoyant");
     }
 
     public void OnExitClick()
     {
-#if Unity_Editor
+
+#if UNITY_EDITOR
+        //Stop playing the scene
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
         Application.Quit();
+        Debug.Log("Quit Application");
     }
 }
